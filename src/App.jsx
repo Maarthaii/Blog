9e2eeeef9cf3d0e375a2a0bb0683308013e8bdc8
Leonardotorre18 <Leonardotorre14@gmail.com>
2023-09-25 20:53:54 -0400

    Restructuración

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,73 @@
+import { Component } from 'react';
 import './App.css';
-import FondoFlorComponent from './FondoFlor';
+import CardBlogComponent from './components/CardBlogComponent';
+import ChangeButtonsComponent from './components/ChangeButtonsComponent';
 
 
 
 
-function MyBlog() {
-  return (
-    <>
-      <div className="contenedor" > 
-      <h1>Jardin de sentimientos</h1>
+class MyBlog extends Component {
 
-      <div  className="contenedores">
-        <img src="../src/img/tulipan1.jpg" className="img-flores" alt="Tulipan"/> 
-        <p  className="info" >El tulipán, delicado y elegante, despliega sus pétalos en un baile armonioso, como un suspiro de amor que se desliza en el viento. Su belleza resplandece como un símbolo de pasión y ternura, cautivando corazones con su encanto cautivador. Cada uno de sus colores vibrantes es un susurro de amor, un regalo de la naturaleza que nos recuerda la dulzura de un romance floreciente.</p> 
-      </div>
+  constructor() {
+    super();
+    this.state = {
+      img1: '/img/fondo-girasoles.jpg',
+      img2: '/img/fondo-girasoles.jpg',
+      img3: '/img/fondo-girasoles.jpg'
+    }
 
-      <div className="contenedores">  
-        <img src="../src/img/dalia1.jpg" className="img-flores" alt="Dalia"/>  
-        <p className="info">La dalia, con su elegancia intrínseca y sus pétalos enigmáticos, es un símbolo de amor que despierta pasiones profundas. Cada curva de sus delicadas formas revela la promesa de un romance floreciente, mientras sus colores vivos despiertan emociones intensas en cada mirada. En el lenguaje secreto de las flores, la dalia susurra palabras de amor eterno y enciende la llama de la pasión en los corazones enamorados.</p> 
-      </div>
+    
+    this.changeImg1URL = this.changeImg1URL.bind(this)
+    this.changeImg2URL = this.changeImg2URL.bind(this)
+    this.changeImg3URL = this.changeImg3URL.bind(this)
+  }
+  changeImg1URL () {
+    this.setState({ img1: '/img/fondo-dalia.jpg' })
+  }
+  changeImg2URL () {
+    this.setState({ img2: '/img/fondo-dalia.jpg' })
+  }
+  changeImg3URL () {
+    this.setState({ img3: '/img/fondo-dalia.jpg' })
+  }
 
-      <div  className="contenedores">
-        <img src="../src/img/girasoles2.jpg" className="img-flores" alt="Girasol"/>  
-        <p  className="info" >El girasol, con su radiante esplendor y su eterna búsqueda del sol, simboliza la belleza que florece en cada mirada suya, mientras sus pétalos siguen con devoción la trayectoria del astro rey. En sus giras hacia la luz, revela un amor eterno que siempre se inclina hacia aquel que ilumina su existencia</p> 
-      </div>
 
-      </div>
-      <br/>
-      <FondoFlorComponent/>
-      
-      
-    </>
-  )
+  render() {
+    return (
+      <>
+        <section className="blog" >
+          <h1 className='blog__title'>Jardin de sentimientos</h1>
+
+
+          <CardBlogComponent imgUrl={this.state.img1}>
+            El tulipán, delicado y elegante, despliega sus pétalos en un baile armonioso, como un suspiro de amor que se desliza en el viento. Su belleza resplandece como un símbolo de pasión y ternura, cautivando corazones con su encanto cautivador. Cada uno de sus colores vibrantes es un susurro de amor, un regalo de la naturaleza que nos recuerda la dulzura de un romance floreciente.
+          </CardBlogComponent>
+  
+
+          <CardBlogComponent imgUrl={this.state.img2}>
+              La dalia, con su elegancia intrínseca y sus pétalos enigmáticos, es un símbolo de amor que despierta pasiones profundas. Cada curva de sus delicadas formas revela la promesa de un romance floreciente, mientras sus colores vivos despiertan emociones intensas en cada mirada. En el lenguaje secreto de las flores, la dalia susurra palabras de amor eterno y enciende la llama de la pasión en los corazones enamorados.
+          </CardBlogComponent>
+  
+
+          <CardBlogComponent imgUrl={this.state.img3}>
+              El girasol, con su radiante esplendor y su eterna búsqueda del sol, simboliza la belleza que florece en cada mirada suya, mientras sus pétalos siguen con devoción la trayectoria del astro rey. En sus giras hacia la luz, revela un amor eterno que siempre se inclina hacia aquel que ilumina su existencia
+          </CardBlogComponent>
+
+
+        </section>
+
+        <ChangeButtonsComponent 
+            events={{
+              changeImg1URL: this.changeImg1URL,
+              changeImg2URL: this.changeImg2URL,
+              changeImg3URL: this.changeImg3URL
+            }}
+
+        ></ChangeButtonsComponent> 
+ 
+      </>
+    )
+  } 
 }
 
 
